b042d676d6f2795bf99956185ecd5fb5ad7ef415
athulnath <athulnath.t@gmail.com>
2015-05-13 13:40:50 +0530

    added func

--- test.js
+++ test.js
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream
-sdjflkjsfdkjlaf 
-adsfaf
-as
-df
-asdf
-
-adsf
-a
-f
-a
-sdf
-adsf
-a
-sdf
-
-3244324234234234234324444444444
-324324
-324
-100000000000000000000000
-
-
-adsfajdsflaksjfdlsdfl dslfkjlaskjflajsdfljafjflak lafdjsadfjlsadjflasj
-=======
 function get_my_serialized_obj(data) {
 	return crypto.createHash('md5').update(data).digest('hex');
 }
->>>>>>> Stashed changes
